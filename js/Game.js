48fbfca592b6a1fa39ecf219d078933fe311c512
BIkramjitSingh123 <72287543+BIkramjitSingh123@users.noreply.github.com>
2021-01-11 19:56:53 -0500

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,24 @@
+class Game{
+    contructor(){
+
+    }
+
+    getState(){
+        var gameStateRef = database.ref("gamestate");
+        gameStateRef.on("value", function(data){
+            gameState = data.val();
+    })}
+    update(state){
+        datebase.ref("/").update({
+            gameState:state
+        })
+    }
+    start(){
+        if(gameState === 0){
+            player = new Player();
+            player.getCount();
+            form = new Form();
+            form.display();
+        }
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas, backgroundImage;
+var form,player,game;
+var gameState = 0;
+var playerCount;
+var database;
+
+function setup(){
+    canvas = createCanvas(400,400);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+
+}
